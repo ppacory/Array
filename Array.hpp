7c20fb477ae3f83e94b2e57b49a722a4d418fb7a
Sumanth <varambally.sumanth@gmail.com>
2013-07-22 18:11:12 +0530

    Added Functionality

--- Array.hpp
+++ Array.hpp
@@ -1,5 +1,31 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2013 Sumanth V
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
+#include <iostream>
+class ARRAY_OUT_OF_RANGE {
+};
 template <typename T>
 class Array {
 public:
@@ -82,7 +108,12 @@ public:
     T& operator[](unsigned int i) {
         return array[i];
     }
-    T& at(unsigned int index) {
+    T& at(unsigned int index) throw(ARRAY_OUT_OF_RANGE) {
+        if(index>=len) {
+            std::cerr<<"Bad index " << index << std::endl;
+            ARRAY_OUT_OF_RANGE a;
+            throw a ;
+        }
         return array[index];
     }
     const T& front() {
@@ -129,6 +160,14 @@ public:
             array=temp;
         }
     }
+
+    void clear() {
+        delete array;
+        maxLen=1;
+        array=new T[maxLen];
+        len=0;
+    }
+
     //*******************************************
 private:
     T* array;
